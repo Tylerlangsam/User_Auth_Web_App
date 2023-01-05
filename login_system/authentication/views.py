b2166c6965778501eac660903c0701c3c5676d27
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-01-04 16:17:37 -0800

    fixing views

--- login_system/authentication/views.py
+++ login_system/authentication/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages 
 from django.contrib.auth import authenticate, login, logout
+from login_system import settings, send_mail
 
 # Create your views here.
 
@@ -19,12 +20,40 @@ def signup(request):
         password = request.POST['password']
         confirmpass = request.POST['confirmpass']
 
+        if User.objects.filter(username=username):
+            messages.error(request, "Username Already Exists")
+            return redirect('home')
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email Already Registered")
+            return redirect('home')
+
+        if len(username)>10:
+            messages.error(request, "Username must be less than 10 characters")
+
+        if password != confirmpass:
+            messages.error(request, "Passwords Do Not Match")
+
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!")
+
         myuser = User.objects.create_user(username, email, password)
         myuser.first_name = firstname
         myuser.last_name = lastname
         myuser.save()
 
         messages.success(request, 'Your Account has been successfully created')
+
+
+        #Welcome Email
+
+        subject = "Welcome to Django Login"
+        message = "Hello" + myuser.first_name + "! \n Welcome to Django! Please confirm your email!"
+
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently = True)
+
 
         return redirect('signin')
     return render(request, "authentication/signup.html")
---
+++ login_system/login_system/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'imap.mail.me.com'
+EMAIL_HOST_USER = 'login_system.django.login@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 993
